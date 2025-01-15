f25cd924d3d6af82db35a0bef08fafa0df437428
Advay Patil <advaypatil27@gmail.com>
2025-01-15 13:53:47 -0800

    Initialize testing with prisma

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -11,8 +11,9 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/$1',
   },
+  setupFilesAfterEnv: ['<rootDir>/__test__/singleton.ts'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
