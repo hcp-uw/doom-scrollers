471c01c59b5ef2654a57e99c0aae57712e5bb2e5
Advayp <69655599+Advayp@users.noreply.github.com>
2025-01-15 23:20:49 -0800

    Initialize frontend (#28)
    * Dockerize backend
    
    * Initialize frontend
    
    * Add ESLint to frontend project

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Tab One</Text>
+    </SafeAreaView>
+  );
+}
