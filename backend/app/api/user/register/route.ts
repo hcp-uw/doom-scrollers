ad420665cdff2babe20329edb8abbb76a5979fe5
Advay Patil <advaypatil27@gmail.com>
2025-01-18 17:56:07 -0800

    Prevent duplicate users in registration

--- backend/app/api/user/register/route.ts
+++ backend/app/api/user/register/route.ts
@@ -1,13 +1,31 @@
 import prisma from '@/lib/prisma';
 import { createSession } from '@/lib/session';
+import { ErrorResponse } from '@/lib/types';
+import { User } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 type PostRequest = {
   username: string;
 };
 
-export const POST = async (req: NextRequest) => {
+type PostResponse = {
+  user: User;
+};
+
+export const POST = async (
+  req: NextRequest
+): Promise<NextResponse<PostResponse | ErrorResponse>> => {
   const { username }: PostRequest = await req.json();
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+  });
+
+  if (existingUser) {
+    return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+  }
 
   const user = await prisma.user.create({
     data: {
