2288b121e835d72f569c105712e6f457f85565aa
Advay Patil <advaypatil27@gmail.com>
2025-01-23 15:59:13 -0800

    Refactor DELETE tests

--- backend/app/api/user/route.test.ts
+++ backend/app/api/user/route.test.ts
@@ -1,16 +1,21 @@
+import { verifySession } from '@/lib/session';
 import { prismaMock } from '@/__test__/singleton';
 import { User } from '@prisma/client';
 import { DELETE } from './route';
 
 jest.mock('@/lib/session', () => ({
-  verifySession: jest.fn(() => ({
-    isAuth: true,
-    uid: 2,
-  })),
-  deleteSession: jest.fn(() => {}),
+  verifySession: jest.fn(),
+  deleteSession: jest.fn(),
 }));
 
+const mockVerifySession = verifySession as jest.Mock;
+
 test('Delete succeeds if user is authenticated', async () => {
+  mockVerifySession.mockResolvedValue({
+    isAuth: true,
+    uid: 1,
+  });
+
   const temporaryUser: User = {
     id: 1,
     username: 'test',
@@ -23,4 +28,16 @@ test('Delete succeeds if user is authenticated', async () => {
   const data = await res.json();
 
   expect(data.success).toBe(true);
+});
+
+test('Delete fails for an invalid session', async () => {
+  mockVerifySession.mockResolvedValue({
+    isAuth: false,
+  });
+
+  const res = await DELETE();
+  const data = await res.json();
+
+  expect(res.status).toEqual(400);
+  expect(data.error).toBe('Invalid session');
 });
