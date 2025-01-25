5208a0b30ddfe4861f2492b1f3f244149ff9e72c
Advay Patil <advaypatil27@gmail.com>
2025-01-24 17:06:25 -0800

    Add routes for fetching and updating genres

---
+++ backend/app/api/genre/add/route.ts
@@ -0,0 +1,51 @@
+import prisma from '@/lib/prisma';
+import { verifySession } from '@/lib/session';
+import { ErrorResponse } from '@/lib/types';
+import { Genre } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+type PutRequest = {
+  genreName: string;
+};
+
+type PutResponse = {
+  genre: Genre;
+};
+
+export const PUT = async (
+  req: NextRequest
+): Promise<NextResponse<PutResponse | ErrorResponse>> => {
+  const { genreName }: PutRequest = await req.json();
+
+  const session = await verifySession();
+
+  if (!session.isAuth) {
+    return NextResponse.json({ error: 'Invalid session' }, { status: 400 });
+  }
+
+  const genre = await prisma.genre.upsert({
+    where: {
+      value: genreName,
+    },
+    create: {
+      value: genreName,
+      users: {
+        connect: {
+          id: session.uid,
+        },
+      },
+    },
+    update: {
+      users: {
+        connect: {
+          id: session.uid,
+        },
+      },
+    },
+    include: {
+      users: true,
+    },
+  });
+
+  return NextResponse.json({ genre });
+};
