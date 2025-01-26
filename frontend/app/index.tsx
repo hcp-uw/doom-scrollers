285290eb096e3ae9832c2b1c590e22d96ef16153
Advayp <69655599+Advayp@users.noreply.github.com>
2025-01-25 21:59:45 -0800

    Add Spotify Auth (#78)
    * Add Spotify Auth
    
    * Don't export `getClientSecret` function

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { SpotifyAuthButton } from '../components/SpotifyAuthButton';
 
 const Index = () => {
   return (
     <SafeAreaView>
       <Text>Hello, World</Text>
+      <SpotifyAuthButton />
     </SafeAreaView>
   );
 };
