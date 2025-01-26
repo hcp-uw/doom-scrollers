a7b03f6b72aa7326153643332e2e7ad8926fad53
Advayp <69655599+Advayp@users.noreply.github.com>
2025-01-25 21:10:01 -0800

    Simplify frontend (#77)
    - Get rid of all the bloat in the frontend
    - Keep only the components necessary for development currently

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
-export default function HomeScreen() {
+const Index = () => {
   return (
     <SafeAreaView>
-      <Text>Tab One</Text>
+      <Text>Hello, World</Text>
     </SafeAreaView>
   );
-}
+};
+
+export default Index;
