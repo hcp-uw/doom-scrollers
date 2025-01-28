814f059ca6f2f09a6e9d16bcc3dfb3126c19f885
Advay Patil <advaypatil27@gmail.com>
2025-01-28 12:18:18 -0800

    Start register page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,32 @@
 import { SpotifyAuthButton } from '@/components/SpotifyAuthButton';
-import { SafeAreaView, Text } from 'react-native';
+import { useRouter } from 'expo-router';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const Index = () => {
+  const router = useRouter();
+
   return (
     <>
-      <SafeAreaView>
-        <Text>Hello, world</Text>
-        <SpotifyAuthButton />
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            router.navigate('/register');
+          }}
+        >
+          <Text>Go To Register Page</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Index;
