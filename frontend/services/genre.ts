45975e9a131147f6c96ce2e43491860516bd42d4
Advayp <69655599+Advayp@users.noreply.github.com>
2025-02-02 14:26:09 -0800

    Add service for managing genres (#105)

---
+++ frontend/services/genre.ts
@@ -0,0 +1,37 @@
+import { LOCAL_API_ENDPOINT } from '@/constants';
+import { Genre } from '@/types';
+import { getCookie } from '@/utils/cookies';
+
+export const getUserGenres = async (): Promise<Genre[]> => {
+  const response = await fetch(`${LOCAL_API_ENDPOINT}/user/`, {
+    headers: {
+      Cookie: (await getCookie())!,
+    },
+  });
+  const data = await response.json();
+  return data.user.preferences as Genre[];
+};
+
+export const selectGenre = async (genreName: string) => {
+  const response = await fetch(`${LOCAL_API_ENDPOINT}/genre/add`, {
+    method: 'PUT',
+    headers: {
+      Cookie: (await getCookie())!,
+    },
+    body: JSON.stringify({ genreName }),
+  });
+  const data = await response.json();
+  return data.genre as Genre;
+};
+
+export const deselectGenre = async (genreName: string) => {
+  const response = await fetch(`${LOCAL_API_ENDPOINT}/genre/remove`, {
+    method: 'PUT',
+    headers: {
+      Cookie: (await getCookie())!,
+    },
+    body: JSON.stringify({ genreName }),
+  });
+  const data = await response.json();
+  return data.genre as Genre;
+};
