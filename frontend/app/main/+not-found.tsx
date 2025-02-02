d024446d57af059d5d09a6f09a0b6f36629292df
Advayp <69655599+Advayp@users.noreply.github.com>
2025-02-02 15:08:07 -0800

    Add tab bar navigation to main page (#107)

---
+++ frontend/app/main/+not-found.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Page does not exist</Text>
+      </SafeAreaView>
+    </>
+  );
+}
