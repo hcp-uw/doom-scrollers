e95bc5e7b7e47dff1824e9788cce0873b159b611
Advay Patil <advaypatil27@gmail.com>
2025-02-02 22:13:26 -0800

    Add initial layout of profile page

--- frontend/app/main/(tabs)/profile.tsx
+++ frontend/app/main/(tabs)/profile.tsx
@@ -1,10 +1,58 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import { useAuth } from '@/hooks/useAuth';
 
 const Profile = () => {
+  const { user, error, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.background}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Loading...</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  if (error) {
+    return (
+      <SafeAreaView style={styles.background}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Error: {error}</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  console.log(user?.createdAt);
+
   return (
     <SafeAreaView style={styles.background}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Profile Page</Text>
+      <View style={styles.profilePictureContainer}>
+        <Image
+          source={{ uri: 'https://i.pravatar.cc/300' }}
+          style={styles.profilePicture}
+        />
+      </View>
+      <View
+        style={{
+          alignItems: 'center',
+          marginBottom: 20,
+        }}
+      >
+        <Text
+          style={[
+            styles.usernameText,
+            {
+              marginBottom: 5,
+            },
+          ]}
+        >
+          @{user?.username}
+        </Text>
+        <Text style={styles.memberSinceText}>
+          member since {user?.createdAt.getFullYear()}
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -16,6 +64,17 @@ const styles = StyleSheet.create({
     height: '100%',
     backgroundColor: 'black',
   },
+  profilePictureContainer: {
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  profilePicture: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderWidth: 8,
+    borderColor: '#a568ff',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
@@ -24,6 +83,18 @@ const styles = StyleSheet.create({
   text: {
     color: 'white',
     fontSize: 24,
+    fontFamily: 'LexendDeca_500Medium',
+  },
+  usernameText: {
+    color: 'white',
+    fontSize: 24,
+    fontFamily: 'LexendDeca_700Bold',
+  },
+  memberSinceText: {
+    color: '#a568ff',
+    fontSize: 16,
+    fontFamily: 'LexendDeca_500Medium',
+    fontStyle: 'italic',
   },
 });
 
