c0b52acb3b14f9868041c2b74572caa1c8b1d1e7
Advay Patil <advaypatil27@gmail.com>
2025-02-03 17:08:41 -0800

    Design/implement genre selection page

--- backend/app/api/genre/route.ts
+++ backend/app/api/genre/route.ts
@@ -1,8 +1,20 @@
 import prisma from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async () => {
   const genres = await prisma.genre.findMany();
 
   return NextResponse.json({ genres });
+};
+
+export const DELETE = async (req: NextRequest) => {
+  const { genreName } = await req.json();
+
+  const genre = await prisma.genre.delete({
+    where: {
+      value: genreName,
+    },
+  });
+
+  return NextResponse.json({ genre });
 };
--- frontend/app/register/index.tsx
+++ frontend/app/register/index.tsx
@@ -37,7 +37,7 @@ const Index = () => {
       return;
     }
 
-    router.navigate('/main');
+    router.navigate('/onboard');
   };
 
   return (
