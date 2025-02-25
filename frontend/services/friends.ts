df3f69ea783f279a4aac9023f3b0143d771771a8
Advayp <69655599+Advayp@users.noreply.github.com>
2025-02-24 23:26:38 -0800

    Add ability to remove friends (#175)

--- frontend/services/friends.ts
+++ frontend/services/friends.ts
@@ -1,5 +1,5 @@
 import { LOCAL_API_ENDPOINT } from '@/constants';
-import { FeedItem } from '@/types';
+import { FeedItem, User } from '@/types';
 import { getCookie } from '@/utils/cookies';
 
 export const getFeed = async (): Promise<FeedItem[]> => {
@@ -11,4 +11,19 @@ export const getFeed = async (): Promise<FeedItem[]> => {
   const body = await response.json();
   console.log(body);
   return body.friends as FeedItem[];
+};
+
+export const removeFriend = async (friendId: number) => {
+  const response = await fetch(`${LOCAL_API_ENDPOINT}/friends/remove`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Cookie: (await getCookie())!,
+    },
+    body: JSON.stringify({ friendId }),
+  });
+
+  const body = await response.json();
+
+  return body.user as User;
 };
